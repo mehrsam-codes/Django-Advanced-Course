52fd391882e833317758be9ffc68754167c06fbf
mehrsam <mehrsamdevelop@gmail.com>
2026-06-14 20:03:55 +0330

    create models
    create models of blog and accounts and create a base user manager model

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog'
+    # 'blog'
 ]
 
 MIDDLEWARE = [
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# user manager config 
+AUTH_USER_MODEL = 'accounts.User'
