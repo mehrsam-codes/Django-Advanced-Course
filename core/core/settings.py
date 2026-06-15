9f3ec194e7db772e413cdeca80a141d77921f7ff
mehrsam <mehrsamdevelop@gmail.com>
2026-06-15 15:51:47 +0330

    setup static and media directory

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    # 'blog'
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT =  BASE_DIR / 'static'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # user manager config 
 AUTH_USER_MODEL = 'accounts.User'
