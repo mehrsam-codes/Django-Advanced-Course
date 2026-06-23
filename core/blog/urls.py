c6600e7b2ffa37ab6b1a8447968c0f9ab862816d
mehrsam <mehrsamdevelop@gmail.com>
2026-06-23 18:44:47 +0330

    working into  cbv views

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -5,6 +5,8 @@ from .views import indexView
 app_name = "blog"
 urlpatterns = [ 
     path('post/' , views.PostListView.as_view() , name = "post-list") ,
-    path('post/<int:pk>/' , views.PostDetailView.as_view() , name="post-detail")
-
+    path('post/<int:pk>/' , views.PostDetailView.as_view() , name="post-detail") , 
+    path('post/create' , views.PostCreateView.as_view() , name='post-create') , 
+    path('post/<int:pk>/edit' , views.PostEditView.as_view() , name='post-edit') ,
+    path('post/<int:pk>/delete' , views.PostDeleteView.as_view(), name='post-delete')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render , redirect
-from django.views.generic import TemplateView , RedirectView , ListView , DetailView
+from django.views.generic import TemplateView , RedirectView , ListView , DetailView , FormView , CreateView , UpdateView ,DeleteView
 from .models import Post
 from django.shortcuts import get_object_or_404
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin  ,PermissionRequiredMixin
 # Create your views here.
 '''
 Funcation Based View Show a Template
@@ -36,7 +38,8 @@ class RedirectToMaktab(RedirectView):
         post = get_object_or_404(Post, pk=kwargs['pk'])
         print(post)
         return super().get_redirect_url(*args, **kwargs)
-class PostListView(ListView):
+class PostListView(PermissionRequiredMixin,LoginRequiredMixin , ListView ):
+    permission_required = 'blog.view_post'
     model = Post
     '''
     or use : queryset = Post.objects.all()
@@ -47,5 +50,37 @@ class PostListView(ListView):
     context_object_name = "post"
     paginate_by = 2
     ordering = '-id' # DESC 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin,DetailView):
     model = Post
+'''
+class PostCreateView(FormView):
+    template_name = 'contact.html'
+    form_class = PostForm
+    success_url = '/blog/post'
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+''' #FormView 
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    '''fields = [
+            'author',
+            'title',
+            'content',
+            'status',
+            'category',
+            'published_date',
+        ]
+        '''
+    form_class = PostForm
+    success_url = '/blog/post'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+class PostEditView(LoginRequiredMixin,UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = '/blog/post/'
