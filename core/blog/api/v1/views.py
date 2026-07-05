ae56fa73a5d5a0f7363950395fb9175ef89d29d1
mehrsam <mehrsamdevelop@gmail.com>
2026-07-05 16:37:41 +0330

    move genericviews to viewsets

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -9,6 +9,7 @@ from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView , ListAPIView ,RetrieveUpdateDestroyAPIView
 from rest_framework.generics import GenericAPIView , ListAPIView , ListCreateAPIView
 from rest_framework import mixins
+from rest_framework import viewsets
 
 '''@api_view(["GET" , "POST"])
 @permission_classes([IsAuthenticated])
@@ -91,3 +92,28 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True) 
+
+class PostViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+    def list(self,request):
+        serializer = self.serializer_class(self.queryset,many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self,request,pk=None):
+        post_object = get_object_or_404(self.queryset , pk=pk)
+        serializer = self.serializer_class(post_object)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
