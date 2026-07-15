811476e205a8215546f385296f4c83123e1779ba
mehrsam <mehrsamdevelop@gmail.com>
2026-07-15 16:56:36 +0330

    adding token for login - and response customation and log out for destroy  token

--- core/accounts/api/v1/serializer.py
+++ core/accounts/api/v1/serializer.py
@@ -1,22 +1,66 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 from ...models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
-class RegistrationSerializer (serializers.ModelSerializer):
-    password1 = serializers.CharField(max_length=255 ,write_only=True)
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(max_length=255, write_only=True)
 
     class Meta:
         model = User
-        fields = ['email' , 'password' , 'password1']
+        fields = ["email", "password", "password1"]
+
     def validate(self, attrs):
-        if attrs.get('password') != attrs.get('password1'):
-            raise serializers.ValidationError({'detail':'passwords does not match'})
+        if attrs.get("password") != attrs.get("password1"):
+            raise serializers.ValidationError({"detail": "passwords does not match"})
         try:
-           validate_password(attrs.get('password'))
+            validate_password(attrs.get("password"))
         except exceptions.ValidationError as e:
-            raise serializers.ValidationError({'password':list(e.messages)})
+            raise serializers.ValidationError({"password": list(e.messages)})
         return super().validate(attrs)
-    
+
     def create(self, validated_data):
-        validated_data.pop('password1',None)
+        validated_data.pop("password1", None)
         return User.objects.create_user(**validated_data)
+
+
+
+class CustomAuthTokenSerializer(serializers.Serializer):
+    email = serializers.CharField(label=_("Email"), write_only=True)
+    password = serializers.CharField(
+        label=_("Password"),
+        style={"input_type": "password"},
+        trim_whitespace=False,
+        write_only=True,
+    )
+    token = serializers.CharField(label=_("Token"), read_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get("email")
+        password = attrs.get("password")
+
+        if username and password:
+            user = authenticate(
+                request=self.context.get("request"),
+                username=username,
+                password=password,
+            )
+
+            # The authenticate call simply returns None for is_active=False
+            # users. (Assuming the default ModelBackend authentication
+            # backend.)
+            if not user:
+                msg = _("Unable to log in with provided credentials.")
+                raise serializers.ValidationError(msg, code="authorization")
+            if not user.is_verified:
+                raise serializers.ValidationError({"details": "user is not verified"})
+        else:
+            msg = _('Must include "username" and "password".')
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'blog' , 
     "rest_framework",
+    'rest_framework.authtoken',
     'django_filters' , 
     'drf_yasg' ,
 ]
@@ -137,4 +138,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ]
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
