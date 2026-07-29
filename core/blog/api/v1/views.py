6019a8c86114b273b880cf3d63184bbea135cc57
mehrsam <mehrsamdevelop@gmail.com>
2026-07-29 13:58:15 +0330

    Update views.py

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,20 +1,31 @@
-from rest_framework.decorators import api_view , permission_classes
-from rest_framework.response import Response 
-from rest_framework.permissions import IsAuthenticatedOrReadOnly , IsAdminUser ,IsAuthenticated
-from .serializers import PostSerializer , CategorySerializer
-from blog.models import Post , Category
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView , ListAPIView ,RetrieveUpdateDestroyAPIView
-from rest_framework.generics import GenericAPIView , ListAPIView , ListCreateAPIView
-from rest_framework import mixins
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from .permission import IsOwnerOrReadOnly
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter , OrderingFilter
+from rest_framework.decorators import action
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework.generics import (
+    GenericAPIView,
+    ListAPIView,
+    ListCreateAPIView,
+)
+from rest_framework.generics import (
+    CreateAPIView,
+    ListAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework.permissions import (
+    IsAuthenticatedOrReadOnly,
+    IsAdminUser,
+    IsAuthenticated,
+)
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from .serializers import PostSerializer, CategorySerializer
+from blog.models import Post, Category
+from .permission import IsOwnerOrReadOnly
 from .paginations import DefaultPagination
+
 # @api_view(["GET" , "POST"])
 # @permission_classes([IsAuthenticated])
 # def postList(request):
@@ -44,8 +55,10 @@ from .paginations import DefaultPagination
         serializer.save()
         return Response(serializer.data)"""
 
+
 class PostList(ListCreateAPIView):
-    '''getting a list of posts and creating a new post'''
+    """getting a list of posts and creating a new post"""
+
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
@@ -91,20 +104,28 @@ class PostList(ListCreateAPIView):
 
 
 class PostDetail(RetrieveUpdateDestroyAPIView):
-    '''getting detail of the post and edit plus removing it '''
+    """getting detail of the post and edit plus removing it"""
+
     permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True) 
+    queryset = Post.objects.filter(status=True)
+
 
 class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend ,SearchFilter,OrderingFilter]
-    filterset_fields = {'category':{"exact","in"}, 'author':{"exact"},'status':{"exact"}}
-    search_fields = ['title', 'content']
-    ordering_fields = ['published_date']
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = {
+        "category": {"exact", "in"},
+        "author": {"exact"},
+        "status": {"exact"},
+    }
+    search_fields = ["title", "content"]
+    ordering_fields = ["published_date"]
     pagination_class = DefaultPagination
+
+
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = CategorySerializer
