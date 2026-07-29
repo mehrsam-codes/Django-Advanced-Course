aac918bab70132700605347c18ecdb2bf3c7890c
mehrsam <mehrsamdevelop@gmail.com>
2026-07-29 13:58:10 +0330

    Update serializers.py

--- core/blog/api/v1/serializers.py
+++ core/blog/api/v1/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from blog.models import Post , Category
+from blog.models import Post, Category
 from accounts.models import Profile
+
+
 # class PostSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
 #     title = serializers.CharField(max_length=255)
@@ -38,18 +40,22 @@ class PostSerializer(serializers.ModelSerializer):
         return request.build_absolute_uri(obj.pk)
 
     def to_representation(self, instance):
-        request = self.context.get('request')
+        request = self.context.get("request")
         rep = super().to_representation(instance)
-        rep['state'] = 'list'
-        if request.parser_context.get('kwargs').get('pk'):
-            rep.pop('snippet' , None)
-            rep.pop('relative_url' , None)
-            rep.pop('absolute_url', None)
+        rep["state"] = "list"
+        if request.parser_context.get("kwargs").get("pk"):
+            rep.pop("snippet", None)
+            rep.pop("relative_url", None)
+            rep.pop("absolute_url", None)
         else:
-            rep.pop('content' , None)
-        rep['category'] = CategorySerializer(instance.category,context={'request':request}).data
+            rep.pop("content", None)
+        rep["category"] = CategorySerializer(
+            instance.category, context={"request": request}
+        ).data
         return rep
-    
+
     def create(self, validated_data):
-        validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
-        return super().create(validated_data) 
+        validated_data["author"] = Profile.objects.get(
+            user__id=self.context.get("request").user.id
+        )
+        return super().create(validated_data)
