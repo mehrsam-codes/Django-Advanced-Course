1284170288515d94397d7d25ba00f78303ead070
mehrsam <mehrsamdevelop@gmail.com>
2026-07-29 13:58:38 +0330

    Update urls.py

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,13 +1,19 @@
-from django.urls import path , include
+from django.urls import path, include
 from . import views
-from django.views.generic import TemplateView , RedirectView
+from django.views.generic import TemplateView, RedirectView
 from .views import indexView
+
 app_name = "blog"
-urlpatterns = [ 
-    path('post/' , views.PostListView.as_view() , name = "post-list") ,
-    path('post/<int:pk>/' , views.PostDetailView.as_view() , name="post-detail") , 
-    path('post/create' , views.PostCreateView.as_view() , name='post-create') , 
-    path('post/<int:pk>/edit' , views.PostEditView.as_view() , name='post-edit') ,
-    path('post/<int:pk>/delete' , views.PostDeleteView.as_view(), name='post-delete') ,
-    path('api/v1/' , include('blog.api.v1.urls'))
-] 
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("post/", views.PostListView.as_view(), name="post-list"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/create", views.PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/edit", views.PostEditView.as_view(), name="post-edit"),
+    path(
+        "post/<int:pk>/delete",
+        views.PostDeleteView.as_view(),
+        name="post-delete",
+    ),
+    path("api/v1/", include("blog.api.v1.urls")),
+]
