9fea8993bd04d95fe310365b63cdce496fa3d8b0
mehrsam <mehrsamdevelop@gmail.com>
2026-07-29 13:57:30 +0330

    Update serializer.py

--- core/accounts/api/v1/serializer.py
+++ core/accounts/api/v1/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ...models import User , Profile
+from ...models import User, Profile
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 from django.contrib.auth import authenticate
@@ -27,7 +27,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop("password1", None)
         return User.objects.create_user(**validated_data)
-
 
 
 class CustomAuthTokenSerializer(serializers.Serializer):
@@ -61,12 +60,14 @@ class CustomAuthTokenSerializer(serializers.Serializer):
         else:
             msg = _('Must include "username" and "password".')
             raise serializers.ValidationError(msg, code="authorization")
-        
+
         attrs["user"] = user
         return attrs
+
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        validated_data =  super().validate(attrs)
+        validated_data = super().validate(attrs)
         username = attrs.get("email")
         password = attrs.get("password")
         user = authenticate(
@@ -76,41 +77,54 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
         )
         if not user.is_verified:
             raise serializers.ValidationError({"details": "user is not verified"})
-        validated_data['email'] = self.user.email 
-        validated_data['user_id'] = self.user.id 
+        validated_data["email"] = self.user.email
+        validated_data["user_id"] = self.user.id
         return validated_data
 
+
 class ChangePasswordSerializer(serializers.Serializer):
-    old_password = serializers.CharField(required =True)
-    new_password = serializers.CharField(required =True)
-    new_password1 = serializers.CharField(required =True)
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    new_password1 = serializers.CharField(required=True)
 
     def validate(self, attrs):
         if attrs.get("new_password") != attrs.get("new_password1"):
-                raise serializers.ValidationError({"detail": "passwords does not match"})
+            raise serializers.ValidationError({"detail": "passwords does not match"})
         try:
             validate_password(attrs.get("new_password"))
         except exceptions.ValidationError as e:
             raise serializers.ValidationError({"new_password": list(e.messages)})
         return super().validate(attrs)
 
+
 class ProfileSerializer(serializers.ModelSerializer):
-    email = serializers.CharField(source='user.email' , read_only=True)
+    email = serializers.CharField(source="user.email", read_only=True)
+
     class Meta:
         model = Profile
-        fields = ('id' ,'email', 'first_name' , 'last_name' , 'image' , 'description')
-        read_only_fields = ['email']
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "image",
+            "description",
+        )
+        read_only_fields = ["email"]
+
 
 class ActivationResendApiSerializer(serializers.Serializer):
-    email = serializers.EmailField(required = True)
+    email = serializers.EmailField(required=True)
 
     def validate(self, attrs):
-        email = attrs.get('email')
+        email = attrs.get("email")
         try:
             user_obj = User.objects.get(email=email)
         except User.DoesNotExist:
-            raise serializers.ValidationError({'details':'user does exist'})
+            raise serializers.ValidationError({"details": "user does exist"})
         if user_obj.is_verified:
-            raise serializers.ValidationError({'details':'user is already active and verified'})
-        attrs['user'] = user_obj
+            raise serializers.ValidationError(
+                {"details": "user is already active and verified"}
+            )
+        attrs["user"] = user_obj
         return super().validate(attrs)
