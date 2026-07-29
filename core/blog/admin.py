51eaa7487d0787ef007ae7b480565041f5dbcb80
mehrsam <mehrsamdevelop@gmail.com>
2026-07-29 13:58:02 +0330

    Update admin.py

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Category , Post
+from .models import Category, Post
+
+
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_diplay = ['author' , 'title' , 'status' , 'category' , 'created_date' , 'published_date']
+    list_diplay = [
+        "author",
+        "title",
+        "status",
+        "category",
+        "created_date",
+        "published_date",
+    ]
+
+
 admin.site.register(Category)
 admin.site.register(Post)
