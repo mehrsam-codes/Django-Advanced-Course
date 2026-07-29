0a38dbd85c50d2f72288ee6d13c08925a3d6a884
mehrsam <mehrsamdevelop@gmail.com>
2026-07-29 13:58:12 +0330

    Update urls.py

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,19 +1,19 @@
-from django.urls import path , include
+from django.urls import path, include
 from . import views
-from rest_framework.routers import DefaultRouter , SimpleRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 
 router = SimpleRouter()
-router.register('post',views.PostModelViewSet,basename='post')
-router.register('category' , views.CategoryModelViewSet , basename='category')
+router.register("post", views.PostModelViewSet, basename="post")
+router.register("category", views.CategoryModelViewSet, basename="category")
 app_name = "api-v1"
 urlpatterns = router.urls
-# urlpatterns = [ 
-    # path('post/' ,views.postList , name="post-list") , 
-    # path('post/<int:id>/' ,views.postDetail , name="post-detail") , 
-#     # path('post/' ,views.PostList.as_view() , name="post-list") , 
-#     # path('post/<int:pk>/',views.PostDetail.as_view() , name="post-detail") , 
-#     path('post/' , views.PostViewSet.as_view({'get':'list','post':'create'}) ,name='post-list' ) , 
-#     path('post/<int:pk>/' , views.PostViewSet.as_view({'get':'retrieve', 'put':'update' , 'patch':'partial_update' , 'delete':'destroy'}) ,       name='post-detail' ) , 
-    # path('',include(router.urls))
-# ] 
-# urlpatterns += router.urls
+urlpatterns = [
+    # path('post/' ,views.postList , name="post-list") ,
+    # path('post/<int:id>/' ,views.postDetail , name="post-detail") ,
+    #     # path('post/' ,views.PostList.as_view() , name="post-list") ,
+    #     # path('post/<int:pk>/',views.PostDetail.as_view() , name="post-detail") ,
+    #     path('post/' , views.PostViewSet.as_view({'get':'list','post':'create'}) ,name='post-list' ) ,
+    #     path('post/<int:pk>/' , views.PostViewSet.as_view({'get':'retrieve', 'put':'update' , 'patch':'partial_update' , 'delete':'destroy'}) ,       name='post-detail' ) ,
+    path("", include(router.urls))
+]
+urlpatterns += router.urls
